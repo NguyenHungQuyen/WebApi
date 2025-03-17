139477d4be11bebd2b8997d1c2b2960408707166
Nguyễn Hùng Quyến <113166528+NguyenHungQuyen@users.noreply.github.com>
2025-03-17 21:15:41 +0700

    Add Microsoft.DataSqlClient

--- obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46e2272c774b6afbd5960eeaa4f7f0bbe35d611c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89609fc761e3b4a2c22c46339e6369cd34d68618")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
